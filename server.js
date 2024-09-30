7849af5a672829f551705ff9162b4e27dbffe86f
Hardish singh <hardishrajpu35@gmail.com>
2024-09-30 17:21:27 +0530

    Updated server.js with new features

--- server.js
+++ server.js
@@ -4,14 +4,22 @@ import Feedback from './models/feedback.js';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import path from 'path';
-
+import dotenv from 'dotenv';
+dotenv.config();
 const app=express();
 const PORT = process.env.PORT || 3000;
 app.use(bodyParser.json());
 app.use(express.json());
 app.use(cors());
 app.use (express.static('public'));
-mongoose.connect('mongodb://localhost:27017/portfolioFeedBack')
+const uri = process.env.MONGODB_URI;
+
+mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then(() => console.log('MongoDB Atlas connected!'))
+.catch(err => console.error('MongoDB connection error:', err));
 app.get('/',(req,res)=>
 {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
