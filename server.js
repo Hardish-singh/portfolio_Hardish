8ac97f5a56b676d7e4d2620068a68e995e1b86ac
Hardish singh <hardishrajpu35@gmail.com>
2024-09-30 20:01:17 +0530

    Updated server.js with new features

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ import path from 'path';
 import dotenv from 'dotenv';
 dotenv.config();
 const app=express();
-const PORT = process.env.PORT || 3000;
+const PORT =  3000;
 app.use(bodyParser.json());
 app.use(express.json());
 app.use(cors());
