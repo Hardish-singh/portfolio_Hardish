acd2f7e12174da9e247d8df74602b07c6083a13a
Hardish singh <hardishrajpu35@gmail.com>
2024-10-01 09:29:17 +0530

    add changes and api

---
+++ api/feedBackForm.js
@@ -0,0 +1,23 @@
+import Feedback from '../../models/feedback.js';
+import mongoose from 'mongoose';
+
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+export default async (req, res) => {
+    if (req.method === 'POST') {
+        const { name, email, message } = req.body;
+        const newFeedback = new Feedback({ name, email, message });
+
+        try {
+            await newFeedback.save();
+            return res.status(200).json({ message: "Form submitted" });
+        } catch (error) {
+            return res.status(400).json({ error: "Internal server error" });
+        }
+    } else {
+        res.status(405).json({ error: "Method not allowed" });
+    }
+};
